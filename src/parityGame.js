23be3921b2109491066852c7277d2a5f342ed796
Shi0-X <jshi0x@gmail.com>
2024-08-26 10:30:40 -0500

    Optimizacion codigo paritygame
    Se optimiza codigo de paritygame para codeclimate reduciendo complejidad cognitiva

--- src/parityGame.js
+++ src/parityGame.js
@@ -6,6 +6,20 @@ const getRandomNumber = () => Math.floor(Math.random() * 100);
 // Función para determinar si un número es par
 const isEven = (number) => number % 2 === 0;
 
+// Función para obtener la respuesta del usuario
+const getUserAnswer = () => readlineSync.question('Tu respuesta: ').toLowerCase();
+
+// Función para verificar la respuesta del usuario
+const checkAnswer = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) {
+    console.log('¡Correcto!');
+    return true;
+  } else {
+    console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
+    return false;
+  }
+};
+
 // Función principal para jugar el juego
 const playParityGame = () => {
   console.log('¡Bienvenido a Brain Games!');
@@ -14,25 +28,30 @@ const playParityGame = () => {
   console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
 
   let correctAnswers = 0;
+  let incorrectAnswers = 0;
+  const maxCorrectAnswers = 3;
+  const maxIncorrectAnswers = 3;
 
-  while (correctAnswers < 3) {
+  while (correctAnswers < maxCorrectAnswers && incorrectAnswers < maxIncorrectAnswers) {
     const number = getRandomNumber();
     console.log(`Pregunta: ${number}`);
-    const userAnswer = readlineSync.question('Tu respuesta: ').toLowerCase();
+    const userAnswer = getUserAnswer();
 
     const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-    if (userAnswer === correctAnswer) {
-      console.log('¡Correcto!');
+    
+    if (checkAnswer(userAnswer, correctAnswer)) {
       correctAnswers += 1;
     } else {
-      console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
+      incorrectAnswers += 1;
       console.log(`¡Intentémoslo de nuevo, ${name}!`);
-      correctAnswers = 0; // Reinicia el conteo de respuestas correctas
     }
   }
 
-  console.log(`¡Felicidades, ${name}!`);
+  if (correctAnswers === maxCorrectAnswers) {
+    console.log(`¡Felicidades, ${name}, Terminaste el juego!`);
+  } else {
+    console.log(`Lo siento, ${name}. Has fallado ${incorrectAnswers} veces. Recuerda que siempre puedes intentarlo de nuevo.`);
+  }
 };
 
 export default playParityGame;
