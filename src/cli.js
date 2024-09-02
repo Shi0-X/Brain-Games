ffc7faa999e076376c827ed8624665a0f0dc3661
Shi0-X <jshi0x@gmail.com>
2024-09-02 14:53:36 -0500

    cli

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
-// src/cli.js
 import readlineSync from 'readline-sync';
 
 const greetUser = () => {
   console.log('¡Bienvenido a Brain Games!');
-  const name = readlineSync.question('¿Cual es tu nombre? ');
+  const name = readlineSync.question('¿Cuál es tu nombre? '); // Corregido para usar acento en la '¿Cuál'
   console.log(`¡Hola, ${name}!`);
 };
 
 export default greetUser;
+
