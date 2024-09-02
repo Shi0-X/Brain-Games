6aaa4584fcf8afe6c6d50a553317cb6ca5bd3592
Shi0-X <jshi0x@gmail.com>
2024-09-02 17:58:17 -0500

    gcd

--- src/gcd.js
+++ src/gcd.js
@@ -8,12 +8,12 @@ let fallas = 0;
 const calcularMCD = (a, b) => (b === 0 ? a : calcularMCD(b, a % b));
 
 // Generar un array de preguntas con números aleatorios y sus MCDs
-const generarPreguntas = (cantidad) =>
-  Array.from({ length: cantidad }, () => { 
+const generarPreguntas = (cantidad) => 
+  Array.from({ length: cantidad }, () => {
     const num1 = Math.floor(Math.random() * 100) + 1;
     const num2 = Math.floor(Math.random() * 100) + 1;
     return { num1, num2, mcd: calcularMCD(num1, num2) };
-  });
+  }); // Eliminados los espacios al final de la línea
 
 // Solicitar una respuesta al usuario
 const solicitarRespuesta = (pregunta) => readlineSync.question(`Pregunta: ${pregunta.num1} ${pregunta.num2}\nTu respuesta: `);
