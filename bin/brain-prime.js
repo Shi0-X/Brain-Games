bfcb5583b8261f36b12ac561d10a0da573a21a61
Shi0-X <jshi0x@gmail.com>
2024-09-03 11:47:28 -0500

    se actualiza brain-prime, para importar funcion de saludo desde src/index.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import runGame from '../src/index.js';
 
+// Función para verificar si un número es primo
 function esPrimo(num) {
   if (num <= 1) return false;
   for (let i = 2; i * i <= num; i += 1) {
@@ -10,65 +11,23 @@ function esPrimo(num) {
   return true;
 }
 
-function obtenerRespuestaUsuario(num) {
-  return readlineSync.question(`Pregunta: ${num}\nTu respuesta: `).toLowerCase();
-}
-
-function validarRespuesta(respuesta, num) {
-  return (respuesta === 'yes' && esPrimo(num)) || (respuesta === 'no' && !esPrimo(num));
-}
-
-function mostrarResultadoFinal(nombre, aciertos) {
-  if (aciertos === 3) {
-    console.log(`¡Felicidades, ${nombre}!`);
-  } else {
-    console.log(`¡Intentémoslo de nuevo, ${nombre}!`);
-  }
-}
-
-function mostrarIntroduccion() {
-  console.log('¡Bienvenido a Brain Games!');
-  const nombre = readlineSync.question('¿Cuál es tu nombre? ');
-  console.log(`¡Hola, ${nombre}!`);
-  console.log('Responde "yes" si el número dado es primo. De lo contrario, responde "no".');
-  return nombre;
-}
-
+// Función para generar un número aleatorio
 function generarNumeroAleatorio() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function procesarRonda() {
+// Función que genera una pregunta y respuesta correcta para el juego
+const gameLogic = () => {
   const num = generarNumeroAleatorio();
-  const respuesta = obtenerRespuestaUsuario(num);
-  const esRespuestaCorrecta = validarRespuesta(respuesta, num);
-
+  const respuestaCorrecta = esPrimo(num) ? 'yes' : 'no';
+  
   return {
-    num,
-    respuesta,
-    esRespuestaCorrecta,
+    question: `${num}`,
+    answer: respuestaCorrecta,
   };
-}
+};
 
-function jugar() {
-  const nombre = mostrarIntroduccion();
+const description = 'Responde "yes" si el número dado es primo. De lo contrario, responde "no".';
 
-  let aciertos = 0;
-  let fallas = 0;
-
-  while (aciertos < 3 && fallas < 1) {
-    const { num, respuesta, esRespuestaCorrecta } = procesarRonda();
-
-    if (esRespuestaCorrecta) {
-      console.log('¡Correcto!');
-      aciertos += 1; // Reemplazo de `aciertos++` por `aciertos += 1`
-    } else {
-      console.log(`'${respuesta}' es incorrecto ;(. La respuesta correcta era '${esPrimo(num) ? 'yes' : 'no'}'.`);
-      fallas += 1; // Reemplazo de `fallas++` por `fallas += 1`
-    }
-  }
-
-  mostrarResultadoFinal(nombre, aciertos);
-}
-
-jugar();
+// Ejecutar el juego utilizando la función runGame
+runGame(description, gameLogic);
