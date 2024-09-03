af16839817d8bce8fd20f80a996f77ac8fd74f75
Shi0-X <jshi0x@gmail.com>
2024-09-03 11:39:45 -0500

    se modifica codigo paritygame para importar funcion de saludo desde index.js

--- src/parityGame.js
+++ src/parityGame.js
@@ -1,4 +1,6 @@
-import readlineSync from 'readline-sync';
+// paritygame.js
+
+import runGame from './index.js'; // Importar runGame desde index.js
 
 // Función para generar un número aleatorio
 const getRandomNumber = () => Math.floor(Math.random() * 100);
@@ -6,45 +8,17 @@ const getRandomNumber = () => Math.floor(Math.random() * 100);
 // Función para determinar si un número es par
 const isEven = (number) => number % 2 === 0;
 
-// Función para obtener la respuesta del usuario
-const getUserAnswer = () => readlineSync.question('Tu respuesta: ').toLowerCase();
+// Lógica específica del juego para verificar la paridad de un número
+const gameLogic = () => {
+  const number = getRandomNumber();
+  const question = `${number}`;
+  const answer = isEven(number) ? 'yes' : 'no'; // Responde "yes" si es par, "no" si no lo es
 
-// Función para verificar la respuesta del usuario
-const checkAnswer = (userAnswer, correctAnswer) => {
-  if (userAnswer === correctAnswer) {
-    console.log('¡Correcto!');
-    return true;
-  }
-  console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
-  return false;
+  return { question, answer };
 };
 
-// Función principal para jugar el juego
-const playParityGame = () => {
-  console.log('¡Bienvenido a Brain Games!');
-  const name = readlineSync.question('¿Cuál es tu nombre? ');
-  console.log(`¡Hola, ${name}!`);
-  console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
+// Descripción del juego
+const description = 'Responde "yes" si el número es par, de lo contrario responde "no".';
 
-  let correctAnswers = 0;
-  const maxCorrectAnswers = 3;
-
-  while (correctAnswers < maxCorrectAnswers) {
-    const number = getRandomNumber();
-    console.log(`Pregunta: ${number}`);
-    const userAnswer = getUserAnswer();
-
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-    if (checkAnswer(userAnswer, correctAnswer)) {
-      correctAnswers += 1;
-    } else {
-      console.log(`¡Intentémoslo de nuevo, ${name}!`);
-      return; // Finaliza el juego inmediatamente después de un error
-    }
-  }
-
-  console.log(`¡Felicidades, ${name}, terminaste el juego con 3 respuestas correctas!`);
-};
-
-export default playParityGame;
+// Exportar la función del juego usando runGame
+export default () => runGame(description, gameLogic);
